eee3b82bc97579dc12ef8147933d74ea2e7451c1
mik-305 <mik-305@bk.ru>
2024-08-01 09:39:39 +0300

    Add files via upload

--- module1hard.py.py
+++ module1hard.py.py
@@ -1,17 +1,17 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]  # Это СПИСОК с оценками
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}                        # Это МНОЖЕСТВО студентов
-students_sort = sorted(students)                         # Отсортированое МНОЖЕСТВО студентов
-nom_student = 0                                          # Счетчик номера студента
-ocenka_odna = ' '                                         # Одна оценка - переменная для подсчёта среднего
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]  # ќв® ‘Џ€‘ЋЉ б ®жҐ­Є ¬Ё
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}                        # ќв® ЊЌЋ†…‘’‚Ћ бвг¤Ґ­в®ў
+students_sort = sorted(students)                         # Ћвб®авЁа®ў ­®Ґ ЊЌЋ†…‘’‚Ћ бвг¤Ґ­в®ў
+nom_student = 0                                          # ‘зҐвзЁЄ ­®¬Ґа  бвг¤Ґ­в 
+ocenka_odna = ' '                                         # Ћ¤­  ®жҐ­Є  - ЇҐаҐ¬Ґ­­ п ¤«п Ї®¤бзсв  баҐ¤­ҐЈ®
 while nom_student <= 4:
     kol_ocenok = 1
     sred_ocenka = 0
     ocenka_all = grades[nom_student]
-    print(students_sort[nom_student], ' < Все его оценки > ', grades[nom_student])
+    print(students_sort[nom_student], ' < ‚бҐ ҐЈ® ®жҐ­ЄЁ > ', grades[nom_student])
     while kol_ocenok <= len(ocenka_all):
-        ocenka_odna = ocenka_all[kol_ocenok-1:kol_ocenok:1] # Извлечение одной оценки
+        ocenka_odna = ocenka_all[kol_ocenok-1:kol_ocenok:1] # €§ў«ҐзҐ­ЁҐ ®¤­®© ®жҐ­ЄЁ
         sred_ocenka = sred_ocenka + int(ocenka_odna.pop(0))
         kol_ocenok = kol_ocenok + 1
     nom_student = nom_student + 1
-    print('Средняя оценка - ',sred_ocenka / (kol_ocenok-1))
+    print('‘аҐ¤­пп ®жҐ­Є  - ',sred_ocenka / (kol_ocenok-1))
 
